c3fb16dce67abf409eb81b4c74e04ca8f7daecee
SinaParvaresh <60529544+SinaParvaresh@users.noreply.github.com>
2023-04-05 15:58:45 -0500

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SinasWebApp.Models;
+
+namespace SinasWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<SinasWebApp.Models.Joke>? Joke { get; set; }
+    }
+}
